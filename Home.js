e449263f9b2dcb87fb4b9de41ed230eda8a22aaa
Ian Ditsch <IanDitsch@Outlook.com>
2019-07-07 00:42:28 -0500

    React Router Activity

---
+++ Home.js
@@ -0,0 +1,19 @@
+// import React from 'react'; 
+
+// const Home= () => (
+//     <div>
+//         <h2>Home</h2>
+//     </div>
+// );
+
+// export default Home;
+
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+export default Home;
---
+++ Topic.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// const Topic = ({ match }) => (
+//     <div>
+//         <h3>{match.params.topicId}</h3>
+//     </div>
+// ); 
+
+// export default Topic; 
+
+import React from 'react';
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+export default Topic;
